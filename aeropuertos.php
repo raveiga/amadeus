ff89831d86bb1512539b70d3e2d70b64f32aad38
Rafa Veiga <profesorveiga@gmail.com>
2013-04-04 15:48:24 +0200

    Posicionamiento de origen y destino y trazado de ruta e información de vuelo y botones vuelos tiempo real.

--- aeropuertos.php
+++ aeropuertos.php
@@ -27,6 +27,23 @@
         z-index:99;
     }
 
+    #infovuelos{
+        width:600px;
+        height:300px;
+        position: absolute;
+        top: 150px;
+        left:150px;
+        padding: 5px;
+        background-color: white;
+        border: 1px solid #A6A6A6;
+        box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+        padding: 10px;
+        vertical-align: middle;
+        white-space: nowrap;
+        font-size: smaller;
+        z-index:99;
+    }
+
     #zonasugerencias{
         position: absolute;
         z-index: 30;
@@ -60,13 +77,13 @@
                     Origen: <input type="text" name="origen" id="origen"/>
                     Destino: <input type="text" name="destino" id="destino"/>
                     <input type="button" name="info" id="info" value="Mostrar Info. Ruta"/><br/><br/>
-                    
+
                     <input type="button" name="aeropuertos" id="aeropuertos" value="Cargar Aeropuertos"/><br/><br/>
-                    
+
                     Origen: <input type="radio" name="desde" id="dorigen" value="origen" checked />Destino: <input type="radio" name="desde" id="ddestino" value="destino" />
                     Salidas: <input type="radio" name="tipo" id="tiposalidas" value="salidas" checked />Llegadas: <input type="radio" name="tipo" id="tipollegadas" value="llegadas"/>
                     <input type="button" name="vuelos" id="vuelos" value="Vuelos en Tiempo Real"/><br/><br/>
-                    
+
                     <input type="button" name="geolocalizar" id="geolocalizar" value="Geolocalizame !"/>
                 </p>
             </div>
@@ -79,6 +96,7 @@
             <h2>Mapas de Aeropuertos</h2>
             <div id="mimapa"></div>
             <div id="infomapa"></div>
+            <div id="infovuelos"></div>
         </div>
     </div><!--end of grids-->
 </div>
