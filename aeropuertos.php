5c0d61d9e3c2da5d7a40f6b8860d330c315d6a49
Rafa Veiga <profesorveiga@gmail.com>
2013-04-05 01:45:58 +0200

    Muestra de salidas y llegadas en tiempo real y posicionamiento de aviones en llegadas.

--- aeropuertos.php
+++ aeropuertos.php
@@ -91,7 +91,7 @@
                     <input type="button" name="aeropuertos" id="aeropuertos" value="Cargar Aeropuertos"/><br/><br/>
 
                     Origen: <input type="radio" name="desde" id="dorigen" value="origen" checked />Destino: <input type="radio" name="desde" id="ddestino" value="destino" />
-                    Salidas: <input type="radio" name="tipo" id="tiposalidas" value="salidas" />Llegadas: <input type="radio" name="tipo" id="tipollegadas" value="llegadas" checked />
+                    Salidas: <input type="radio" name="tipo" id="tiposalidas" value="salidas" checked />Llegadas: <input type="radio" name="tipo" id="tipollegadas" value="llegadas" />
                     <input type="button" name="vuelos" id="vuelos" value="Vuelos en Tiempo Real"/><br/><br/>
 
                     <input type="button" name="geolocalizar" id="geolocalizar" value="Geolocalizame !"/>
