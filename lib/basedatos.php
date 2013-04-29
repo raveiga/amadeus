60584a21c4322f7ce81be6477a4282fe699e55e0
Rafa Veiga <profesorveiga@gmail.com>
2013-04-29 15:44:09 +0200

    Incorporación de libreria twitterOAuth y modificaciones de métodos ChequearNick e InsertarUsuario en basedatos.php

--- lib/basedatos.php
+++ lib/basedatos.php
@@ -78,40 +78,48 @@ class Basedatos {
      * @param string $telefono
      * @return string "OK" indicando que se ha realizado con éxito la insercción de datos.
      */
-    public function insertarUsuario($nick, $password, $nombre, $apellidos, $dni, $email, $telefono, $tipo = 'local') {
-        // Preparamos la instrucción SQL.
-        $stmt = self::$_mysqli->prepare("insert into amadeus_usuarios(nick,password,nombre,apellidos,dni,email,telefono,token) values(?,?,?,?,?,?,?,?)") or die(self::$_mysqli->error);
+    public function insertarUsuario($nick, $password, $nombre, $apellidos, $dni, $email, $telefono, $tipo = 'local', $atoken = 'null', $atsecret = '') {
+        switch ($tipo) {
+            case 'local':
+                // Preparamos la instrucción SQL.
+                $stmt = self::$_mysqli->prepare("insert into amadeus_usuarios(nick,password,nombre,apellidos,dni,email,telefono,token) values(?,?,?,?,?,?,?,?)") or die(self::$_mysqli->error);
+                // Enlazamos los parámetros.
+                $nick = strtolower($nick);
+                $encriptada = encriptar($password, 10);
+                $token = md5($encriptada);
+                $stmt->bind_param('ssssssss', $nick, $encriptada, $nombre, $apellidos, $dni, $email, $telefono, $token);
 
-        if ($tipo == 'local') {
-            // Enlazamos los parámetros.
-            $nick = strtolower($nick);
-            $encriptada = encriptar($password, 10);
-            $token = md5($encriptada);
-            $stmt->bind_param('ssssssss', $nick, $encriptada, $nombre, $apellidos, $dni, $email, $telefono, $token);
+                // Ejecutamos la instrucción
+                $stmt->execute() or die(self::$_mysqli->error);
 
-            // Ejecutamos la instrucción
-            $stmt->execute() or die(self::$_mysqli->error);
+                $contenido = "Estimado señor/a $nombre $apellidos.<br/><br/>Hemos recibido una petición de registros en nuestra web de viajes Amadeus.";
+                $contenido.="Si usted no ha realizado dicha petición, simplemente borre este correo y en breve el registro será borrado de nuestra base de datos.<br/><br/>";
+                $contenido.="En otro caso, confirme su registro antes de 24 H en la siguiente dirección de Amadeus:<br/>";
+                $contenido.="<a href='" . Config::$urlAplicacion . "/confirmar.html?nick=$nick&token=$token'>Confirmación registro en web viajes Amadeus</a><br/><br/>";
+                $contenido.="IP registrada: " . obtenerIP() . "<br/><br/>";
+                $contenido.="Reciba un cordial saludo.<br/><br/>Agencia de viajes Amadeus &copy; 2013.";
 
-            $contenido = "Estimado señor/a $nombre $apellidos.<br/><br/>Hemos recibido una petición de registros en nuestra web de viajes Amadeus.";
-            $contenido.="Si usted no ha realizado dicha petición, simplemente borre este correo y en breve el registro será borrado de nuestra base de datos.<br/><br/>";
-            $contenido.="En otro caso, confirme su registro antes de 24 H en la siguiente dirección de Amadeus:<br/>";
-            $contenido.="<a href='" . Config::$urlAplicacion . "/confirmar.html?nick=$nick&token=$token'>Confirmación registro en web viajes Amadeus</a><br/><br/>";
-            $contenido.="IP registrada: " . obtenerIP() . "<br/><br/>";
-            $contenido.="Reciba un cordial saludo.<br/><br/>Agencia de viajes Amadeus &copy; 2013.";
+                if (enviarCorreo($nombre . ' ' . $apellidos, $email, 'Confirmación registro en Viajes Amadeus', $contenido))
+                    return "Registro realizado correctamente.<br/><br/>Le hemos enviado una confirmación a su correo electrónico:<br/>$email";
+                else
+                    return "!! ATENCION !!<br/><br/>Se ha producido un fallo al enviar el correo a $email.<br/>Contacte con " . Config::$mailEmailRemitente . " para informar del problema.";
+                break;
 
-            if (enviarCorreo($nombre . ' ' . $apellidos, $email, 'Confirmación registro en Viajes Amadeus', $contenido))
-                return "Registro realizado correctamente.<br/><br/>Le hemos enviado una confirmación a su correo electrónico:<br/>$email";
-            else
-                return "!! ATENCION !!<br/><br/>Se ha producido un fallo al enviar el correo a $email.<br/>Contacte con " . Config::$mailEmailRemitente . " para informar del problema.";
-        }
-        else if ($tipo == 'ldap') {
-            // Enlazamos los parámetros.
-            $nick = strtolower($nick);
-            $encriptada = encriptar($nick, 9);
-            $token = 'ldap';
-            $stmt->bind_param('ssssssss', $nick, $encriptada, $nombre, $apellidos, $dni, $email, $telefono, $token);
-            // Ejecutar la SQL.
-            $stmt->execute() or die(self::$_mysqli->error);
+            case 'ldap':
+                // Preparamos la instrucción SQL.
+                $stmt = self::$_mysqli->prepare("insert into amadeus_usuarios(nick,password,nombre,apellidos,dni,email,telefono,token) values(?,?,?,?,?,?,?,?)") or die(self::$_mysqli->error);
+                // Enlazamos los parámetros.
+                $nick = strtolower($nick);
+                $encriptada = encriptar($nick, 9);
+                $token = 'ldap';
+                $stmt->bind_param('ssssssss', $nick, $encriptada, $nombre, $apellidos, $dni, $email, $telefono, $token);
+                // Ejecutar la SQL.
+                $stmt->execute() or die(self::$_mysqli->error);
+                break;
+
+            case 'twitter':
+
+                break;
         }
     }
 
@@ -124,7 +132,6 @@ class Basedatos {
      * @param string $nick Nick del usuario.
      * @return string Mensaje de en uso o disponible.
      */
-
     public function chequearNick($nick) {
         // Preparamos la consulta.
         $stmt = self::$_mysqli->prepare("SELECT * from amadeus_usuarios where nick=?") or (self::$_mysqli->error);
@@ -204,80 +211,94 @@ class Basedatos {
      * @param string $pass Contraseña a comprobar.
      * @return string Mensaje de confirmación. OK si todo fue correcto.
      */
-    public function chequearAcceso($nick, $pass, $autenticacion) {
-        if ($autenticacion == 'local') {
-            // Preparamos la consulta.
-            $nick = strtolower($nick);
+    public function chequearAcceso($nick, $pass, $autenticacion = 'local') {
+        switch ($autenticacion) {
+            case 'local':
+                // Preparamos la consulta.
+                $nick = strtolower($nick);
 
-            $stmt = self::$_mysqli->prepare("select nombre, apellidos, password, token, fotografia from amadeus_usuarios where nick=?") or die(self::$_mysqli->error);
+                $stmt = self::$_mysqli->prepare("select nombre, apellidos, password, token, fotografia from amadeus_usuarios where nick=?") or die(self::$_mysqli->error);
 
-            $stmt->bind_param("s", $nick);
+                $stmt->bind_param("s", $nick);
 
-            // Ejecutamos la consulta
-            $stmt->execute();
+                // Ejecutamos la consulta
+                $stmt->execute();
 
-            // Es una consulta select.... almacenamos el resultado.
-            $stmt->store_result();
+                // Es una consulta select.... almacenamos el resultado.
+                $stmt->store_result();
 
-            $stmt->bind_result($nombre, $apellidos, $password, $token, $fotografia);
+                $stmt->bind_result($nombre, $apellidos, $password, $token, $fotografia);
 
-            // Número de filas obtenidas
-            $numfilas = $stmt->num_rows;
+                // Número de filas obtenidas
+                $numfilas = $stmt->num_rows;
 
-            if ($numfilas == 1) {
-                // Leemos la fila del recordset
-                $stmt->fetch();
+                if ($numfilas == 1) {
+                    // Leemos la fila del recordset
+                    $stmt->fetch();
 
-                if ($token != 'OK')
-                    if ($token == 'ldap')
-                        return 'Su cuenta de usuario es una cuenta LDAP.<br/>!! Valídese en el dominio !!';
+                    if ($token != 'OK')
+                        if ($token == 'ldap')
+                            return 'Su cuenta de usuario es una cuenta LDAP.<br/>!! Valídese en el dominio !!';
+                        else
+                            return 'Su e-mail no fue validado. Compruebe su buzón';
+
+                    // Comprobamos la contraseña
+                    if (crypt($pass, $password) == $password) {
+                        // Creamos las variables de sesión para el usuario conectado.
+                        $_SESSION['usuario'] = $nick;
+                        $_SESSION['nombre'] = $nombre;
+                        $_SESSION['apellidos'] = $apellidos;
+                        $_SESSION['fotografia'] = $fotografia;
+                        $_SESSION['token'] = $token;
+                        return "OK";
+                    }
                     else
-                        return 'Su e-mail no fue validado. Compruebe su buzón';
-
-                // Comprobamos la contraseña
-                if (crypt($pass, $password) == $password) {
-                    // Creamos las variables de sesión para el usuario conectado.
-                    $_SESSION['usuario'] = $nick;
-                    $_SESSION['nombre'] = $nombre;
-                    $_SESSION['apellidos'] = $apellidos;
-                    $_SESSION['fotografia'] = $fotografia;
-                    $_SESSION['token'] = $token;
-                    return "OK";
+                        return "ERROR: datos de acceso incorrectos.";
                 }
                 else
                     return "ERROR: datos de acceso incorrectos.";
-            }
-            else
-                return "ERROR: datos de acceso incorrectos.";
-        }
-        else if ($autenticacion = 'ldap') {
-            // Cargamos la clase ldap.
-            require_once dirname(__FILE__) . '/ldap.php';
+                break;
 
-            // Creamos un objeto de tipo ldap
-            $ldap = new ldap(Config::$ldapServidor, Config::$ldapPuerto);
-            if ($ldap->validarUsuario("$nick", Config::$ldapDominio, "$pass")) {
-                // Obtenemos la información del usuario en el LDAP.
-                $datos = $ldap->infoUsuariO("$nick");
 
-                // Creamos las variables de sesión.
-                $_SESSION['nombre'] = $ldap->name;
-                $_SESSION['apellidos'] = $ldap->displayname;
-                $_SESSION['email'] = $ldap->mail;
-                $_SESSION['usuario'] = $ldap->name;
-                $_SESSION['token'] = 'ldap';
+            case 'ldap':
+                // Cargamos la clase ldap.
+                require_once dirname(__FILE__) . '/ldap.php';
 
-                if ($this->chequearNick("$nick") == 'Nick disponible') 
-                {   // El usuario no está creado en la base de datos, lo creamos en modo local.
-                    $this->insertarUsuario($nick, '', $ldap->name, $ldap->displayname, '', $ldap->mail, '', 'ldap');
+                // Creamos un objeto de tipo ldap
+                $ldap = new ldap(Config::$ldapServidor, Config::$ldapPuerto);
+                if ($ldap->validarUsuario("$nick", Config::$ldapDominio, "$pass")) {
+                    // Obtenemos la información del usuario en el LDAP.
+                    $datos = $ldap->infoUsuariO("$nick");
+
+                    // Creamos las variables de sesión.
+                    $_SESSION['nombre'] = $ldap->name;
+                    $_SESSION['apellidos'] = $ldap->displayname;
+                    $_SESSION['email'] = $ldap->mail;
+                    $_SESSION['usuario'] = $ldap->name;
+                    $_SESSION['token'] = 'ldap';
+
+                    if ($this->chequearNick("$nick") == 'Nick disponible') {   // El usuario no está creado en la base de datos, lo creamos en modo local.
+                        $this->insertarUsuario($nick, '', $ldap->name, $ldap->displayname, '', $ldap->mail, '', 'ldap');
+                    }
+                    else
+                        return "ERROR: El modo de autenticación para este usuario<br/>no es el correcto.";
+
+                    return "OK";
+                }
+                else
+                    echo "ERROR LDAP: datos incorrectos de acceso.";
+                break;
+
+
+            case 'twitter':
+                if ($this->chequearNick("$nick") == 'Nick disponible') {   // El usuario no está creado en la base de datos, lo insertamos en modo local.
+                    $this->insertarUsuario($nick, '', $_SESSION['nombre'], '', '', '', '', 'twitter');
                 }
                 else
                     return "ERROR: El modo de autenticación para este usuario<br/>no es el correcto.";
 
                 return "OK";
-            }
-            else
-                echo "ERROR LDAP: datos incorrectos de acceso.";
+                break;
         }
     }
 
@@ -357,8 +378,7 @@ class Basedatos {
         else
             $token = $_SESSION['token'];
 
-        if ($_SESSION['token'] != 'ldap') 
-        {
+        if ($_SESSION['token'] != 'ldap') {
             // Preparamos la instrucción SQL.
             $stmt = self::$_mysqli->prepare("update amadeus_usuarios set password=?, nombre=?, apellidos=?, dni=?, email=?, telefono=?, token=? where nick=?") or die(self::$_mysqli->error);
 
@@ -382,9 +402,8 @@ class Basedatos {
                     return "!! ATENCION !!<br/><br/>Se ha producido un fallo al enviar el correo a $email.<br/>Contacte con " . Config::$mailEmailRemitente . " para informar del problema.";
             }
         }
-        else
-        {
-             // Preparamos la instrucción SQL para actualizar los datos de DNI y demás.
+        else {
+            // Preparamos la instrucción SQL para actualizar los datos de DNI y demás.
             $stmt = self::$_mysqli->prepare("update amadeus_usuarios set dni=?, telefono=? where nick=?") or die(self::$_mysqli->error);
 
             // Enlazamos los parámetros.
